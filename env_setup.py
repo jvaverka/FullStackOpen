9d0ce6d5c5f31ac02a7365233792fc9ab50e7d71
JVaverka <jvaverka@protonmail.com>
2020-06-24 20:14:22 -0400

    demo

--- env_setup.py
+++ env_setup.py
@@ -6,15 +6,9 @@ Desired Environment-
 4) git installed / ssh keys configured
 5) full stack open git repository cloned
 """
-import requests
+import socket
 
-conda_url = "https://repo.anaconda.com/archive/Anaconda3-2020.02-MacOSX-x86_64.pkg"
-r = requests.get(conda_url)  # create HTTP response object
-# send a HTTP request to the server and save
-# the HTTP response in a response object called r
-with open("~/Downloads/anaconda_download.pkg", 'wb') as f:
-    # Saving received content in binary format
-    # write the contents of the response (r.content)
-    # to a new file in binary mode.
-    f.write(r.content)
-# run gui installer
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+print(hostname, ip_address)
