0b9838b9194840c172e6e2e67f426c41b552a318
JVaverka <jvaverka@protonmail.com>
2020-08-07 14:10:07 -0400

    exercise 1.6 complete

--- part1ex_unicafe/src/index.js
+++ part1ex_unicafe/src/index.js
@@ -1,15 +1,43 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
+/**
+ * @description display text in header format
+ */
+const Head = ({ text }) => <h1>{text}</h1>
+/**
+ * @description create button with label & callback
+ */
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+/**
+ * @description main app
+ */
+/**
+ * @description show feedback statistics
+ */
+const Stats = ({ good, neutral, bad }) => (
+  <>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </>
+)
 const App = () => {
   // save clicks of each button to own state
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad ] = useState(0)
-
+  // handlers
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
   return (
     <>
-      code here
+      <Head text='give feedback' />
+      <Button handleClick={incrementGood} text='good' />
+      <Button handleClick={incrementNeutral} text='neutral' />
+      <Button handleClick={incrementBad} text='bad' />
+      <Head text='statistics' />
+      <Stats good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
