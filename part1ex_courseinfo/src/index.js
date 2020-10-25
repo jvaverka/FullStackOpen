f13de4bef30d91edc66cf0d5318861ecdbf9a078
J Vaverka <jvaverka@protonmail.com>
2020-10-25 10:47:55 -0400

    exercise 2.4 courseinfo step9 complete - extend app to allow for arbitrary number of courses.

--- part1ex_courseinfo/src/index.js
+++ part1ex_courseinfo/src/index.js
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Course = ({ course }) => {
-  // console.log(course)
   let result = []
-  result.push(<Header key={course.id} name={course.name} />)
+  result.push(<SubHeader key={course.id} name={course.name} />)
   course.parts.forEach(part => {
     result.push(
       <Content key={course.id + part.id} name={part.name} count={part.exercises} />
@@ -16,6 +15,8 @@ const Course = ({ course }) => {
 
 const Header = ({ name }) => <h1>{name}</h1>
 
+const SubHeader = ({ name }) => <h2>{name}</h2>
+
 const Content = ({ name, count }) => <Part name={name} count={count} />
 
 const Part = ({ name, count }) => <p>{name} {count}</p>
@@ -23,7 +24,6 @@ const Part = ({ name, count }) => <p>{name} {count}</p>
 const Total = (props) => {
   
   const totalExercises = props.parts.reduce((sum, part) => {
-    // console.log('hello', sum, part);
     return sum + part.exercises
   }, 0)
 
@@ -35,34 +35,57 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  let result = []
+  result.push(<Header key={0} name='Web Development Cirriculum' />)
+  courses.forEach(course =>
+    result.push(<Course key={course.id} course={course} />)
+  )
+  return result
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
