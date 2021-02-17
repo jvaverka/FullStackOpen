72c6f7d1881d0330701947cec911bcd565325ffa
J Vaverka <jvaverka@protonmail.com>
2021-02-17 18:25:51 -0500

    resync

---
+++ phonebook/src/components/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Person from './Person'
+
+const Content = ({ persons, deletePerson }) =>
+    <ul>
+        {persons.map((person, i) =>
+            <Person
+                key={i}
+                person={person}
+                deletePerson={() => deletePerson(person)}
+            />
+        )}
+    </ul>
+
+export default Content
