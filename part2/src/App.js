20b3f6501f35fa30f0923fe6182e97113c1b3830
J Vaverka <jvaverka@protonmail.com>
2021-02-17 14:39:05 -0500

    complete part 2d - changing the importance of ntoes

--- part2/src/App.js
+++ part2/src/App.js
@@ -43,6 +43,17 @@ const App = () => {
     const notesToShow = showAll
         ? notes
         : notes.filter( note => note.important )
+    
+    const toggleImportanceOf = id => {
+        const url = `http://localhost:3001/notes/${id}`
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        axios.put(url, changedNote)
+            .then(response => {
+                setNotes(notes.map(note => note.id !== id ? note : response.data))
+            })
+    }
 
     return (
         <div>
@@ -54,7 +65,11 @@ const App = () => {
             </div>
             <ul>
                 {notesToShow.map(note =>
-                    <Note key={note.id} note={note} />
+                    <Note 
+                        key={note.id} 
+                        note={note} 
+                        toggleImportance={() => toggleImportanceOf(note.id)}
+                    />
                 )}
             </ul>
             <form onSubmit={addNote}>
