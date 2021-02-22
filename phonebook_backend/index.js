214b1caeac87bb08d530c418f0afc6b3d1a0c362
J Vaverka <jvaverka@protonmail.com>
2021-02-22 18:20:47 -0500

    3.2: Phonebook backend step2 - Implement a page at the address http://localhost:3001/info that shows the time that the request was received and how many entries are in the phonebook at the time of processing the request.

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -3,45 +3,58 @@ const app = express()
 
 app.use(express.json())
 
-const persons = {
-    "persons": [
-        {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-        },
-        {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-        },
-        {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-        },
-        {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122",
-            "id": 4
-        },
-        {
-            "name": "Madison Tankersley",
-            "number": "770-539-4348",
-            "id": 5
-        },
-        {
-            "name": "Jacob Vaverka",
-            "number": "770-530-8275",
-            "id": 6
-        },
-        {
-            "name": "Henry David Thoreau",
-            "number": "unlisted",
-            "id": 7
-        }
-    ]
-}
+const persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
+    {
+        "name": "Madison Tankersley",
+        "number": "770-539-4348",
+        "id": 5
+    },
+    {
+        "name": "Jacob Vaverka",
+        "number": "770-530-8275",
+        "id": 6
+    },
+    {
+        "name": "Henry David Thoreau",
+        "number": "unlisted",
+        "id": 7
+    }
+]
+
+app.get('/info', (req, res) => {
+    const currentDate = new Date().toString()
+    // const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    res.send(
+        `
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+        </div>
+        <div>
+            <p>${currentDate}</p>
+        </div>
+        `
+    )
+})
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
