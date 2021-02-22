787621d170814f0b092341cba894c3f33f42e390
J Vaverka <jvaverka@protonmail.com>
2021-02-22 18:06:21 -0500

    3.1: Phonebook backend step1 - Implement a Node application that returns a hardcoded list of phonebook entries from the address http://localhost:3001/api/persons

---
+++ phonebook_backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const persons = {
+    "persons": [
+        {
+            "name": "Arto Hellas",
+            "number": "040-123456",
+            "id": 1
+        },
+        {
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523",
+            "id": 2
+        },
+        {
+            "name": "Dan Abramov",
+            "number": "12-43-234345",
+            "id": 3
+        },
+        {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122",
+            "id": 4
+        },
+        {
+            "name": "Madison Tankersley",
+            "number": "770-539-4348",
+            "id": 5
+        },
+        {
+            "name": "Jacob Vaverka",
+            "number": "770-530-8275",
+            "id": 6
+        },
+        {
+            "name": "Henry David Thoreau",
+            "number": "unlisted",
+            "id": 7
+        }
+    ]
+}
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
