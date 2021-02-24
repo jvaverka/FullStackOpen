40037fffdcf77e5e18084b3b73bdc265ab9f00f3
J Vaverka <jvaverka@protonmail.com>
2021-02-23 19:33:34 -0500

    3.9 phonebook backend step9 - Make the phonebook backend work with the frontend.

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+
 app.use(morgan((tokens, req, res) => {
     return [
         tokens.method(req, res),
